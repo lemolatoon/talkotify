d9753b2f8120a35b3521af99d51d8284cb0b8b8a
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-06-26 23:33:35 +0900

    Add openai deps

--- src/talkotify/__main__.py
+++ src/talkotify/__main__.py
@@ -3,6 +3,13 @@ from .env import init_env, checked_get
 init_env()
 OPENAI_API_KEY: str = checked_get("OPENAI_API_KEY")
 
+def voice_to_text():
+    audio = get_audio_from_mic()
+    audio_data = BytesIO(audio.get_wav_data())
+    audio_data.name = 'from_mic.wav'
+    transcript = openai.Audio.transcribe('whisper-1', audio_data)
+    return transcript['text']
+
 def run():
     init_env()
     print("hoy!")
